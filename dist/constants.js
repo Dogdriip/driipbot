e1c494367b4a5fdf3e218229b48de2c9b89f6107
Hyunseung Jeon <dogdriip@gmail.com>
2021-02-12 18:45:24 +0900

    gitignore

--- dist/constants.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TYPE = void 0;
-exports.TYPE = {
-    BotInstance: Symbol.for("BotInstance"),
-    Middleware: Symbol.for("Middleware"),
-};
--- dist/middleware/Calculator.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (function () { return function (ctx) {
-    var re = /^[0-9+\-*/^()]+$/g;
-    var text = ctx.message.text.split("=")[0];
-    if (re.test(text)) {
-        ctx.reply(eval(text));
-    }
-}; });
