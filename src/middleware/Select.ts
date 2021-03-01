2393504392d8251fa88ddc0d189a552468ba1a72
Hyunseung Jeon <dogdriip@gmail.com>
2021-03-01 23:58:41 +0900

    Select await

--- src/middleware/Select.ts
+++ src/middleware/Select.ts
@@ -1,5 +1,9 @@
 export default () => async (ctx) => {
-  const { args } = ctx.state.command;
-  const rand_idx: number = Math.floor(Math.random() * args.length);
-  await ctx.reply(`${args[rand_idx]}`);
+  try {
+    const { args } = ctx.state.command;
+    const rand_idx: number = Math.floor(Math.random() * args.length);
+    await ctx.reply(`${args[rand_idx]}`);
+  } catch (e) {
+    console.error(e);
+  }
 };
